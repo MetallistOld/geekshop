c4ae7f46937c8b610f20711abc293f26c9381da4
Anatoliy Maximichev <84544880+MetallistOld@users.noreply.github.com>
2022-02-13 22:27:53 +0300

    Lesson 3 (#3)
    * Lesson 3. Django ORM start
    
    * Lesson 3. Djagno admin
    
    * Lesson 3. Models in context
    
    * Lesson 3. Urls for app
    
    * Lesson 3. Urls for app
    
    * Lesson 3. Code style fix

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
+# Register your models here.
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductCategory
+
+admin.site.register(ProductCategory)
+admin.site.register(Product)
